0a0f75944084b6de68fca3d355b98d3a9c62ddec
James P <gwg6784@aut.ac.nz>
2024-04-28 22:10:24 +1200

    Changes

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class Main : MonoBehaviour
@@ -9,21 +8,18 @@ public class Main : MonoBehaviour
     public Spinner spinner2;
     public Spinner spinner3;
 
-    public Image blocker1;
-    public Image blocker2;
-
     public SFX sfx;
 
     public VideoPlayer winVideo;
 
 
     private bool spinning = false;
-    private bool canPress = false;
+    private bool canPress = true;
     private bool cheating = false;
 
     private void Start()
     {
-        StartCoroutine(AnimateStart());
+        winVideo.Prepare();
     }
 
     public void OnButtonClick()
@@ -39,21 +35,6 @@ public class Main : MonoBehaviour
     public void Cheat()
     {
         cheating = true;
-    }
-
-    private IEnumerator AnimateStart()
-    {
-        yield return new WaitForSeconds(2f);
-        sfx.Play(sfx.spotlight);
-        yield return new WaitForSeconds(0.1f);
-        blocker1.enabled = false;
-
-        yield return new WaitForSeconds(3f);
-        sfx.Play(sfx.spotlight);
-        yield return new WaitForSeconds(0.1f);
-        blocker2.enabled = false;
-
-        canPress = true;
     }
 
     private IEnumerator ToggleSpin()
